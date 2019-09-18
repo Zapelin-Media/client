cee7f19a24073bfc9f5659cb96a8b52b614be28b
Ubaldo Porcheddu <ubaldo@eja.it>
2019-09-18 11:02:27 +0100

    url fix

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 apt-get update
 apt-get install -y eja
 eja --update
-eja --install https://github.com/zapelin-media/zmi/raw/zmi.eja
+eja --install https://github.com/zapelin-media/zmi/raw/master/zmi.eja
 eja --zmi-install --log-level 3
