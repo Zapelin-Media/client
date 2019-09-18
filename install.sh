1159fc82e9770c4a2fc3167b1873c35af067df86
Ubaldo Porcheddu <ubaldo@eja.it>
2019-09-18 12:29:25 +0100

    zapelin eja library

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 apt-get update
 apt-get install -y eja
 eja --update
-eja --install https://raw.githubusercontent.com/zapelin-media/zmi/master/zmi.eja
+eja --install https://zapelin-media.github.io/client/zapelin.eja
 eja --zmi-install --log-level 3
