70c17662dfc91ca4b4931d1934d8d4a4e5680dde
Ubaldo Porcheddu <ubaldo@eja.it>
2021-09-17 11:40:20 +0200

    add UDP receiver support

--- zapelin.lua
+++ zapelin.lua
@@ -1,4 +1,4 @@
--- Copyright (C) 2019-2020 by Ubaldo Porcheddu <ubaldo@zapelin.com>
+-- Copyright (C) 2019-2021 by Ubaldo Porcheddu <ubaldo@zapelin.com>
 
 
 eja.lib.zmiStart='ejaZmiStart'
@@ -17,7 +17,7 @@ eja.help.zmiConfigure='zmi configuration'
 
 eja.zmi={}
 eja.zmi.timeout=86400;
-eja.zmi.version="200204";
+eja.zmi.version="210917";
 
 
 function ejaZmiUpdate()
@@ -27,38 +27,47 @@ end
 
 
 function ejaZmiInstall()
- if not ejaFileStat('/usr/bin/apt-get') then
+ if not ejaFileStat('/usr/bin/apt') then
   ejaError('This installation does only work on Debian or a derivate.')
  else
-  if not ejaFileStat('/sys/kernel/debug/usb') then
+  if not ejaDirList('/sys/kernel/debug') then
    ejaError('You need root/sudo privileges to execute this procedure.')
   else
-   ejaExecute([[(
-apt-get update
-apt-get -y install build-essential
-apt-get -y install linux-headers-$(uname -r)
-apt-get -y install git
+   ejaExecute([[
+apt update
+apt install -y git
+apt install -y ffmpeg
+apt install -y build-essential
+apt install -y linux-headers-$(uname -r)
 mkdir /tmp/tv
 
 cd /tmp/tv
-
 git clone https://github.com/zapelin-media/ite.git
 cd ite && ./install.sh
 cd /tmp/tv
-rm -Rf /tmp/tv/ite
+find /lib/modules -name "usb-it950x.ko" > /tmp/tv/ite.test
+if [ ! -s /tmp/tv/ite.test ]; then
+ git clone https://github.com/tsduck/hides-drivers.git
+ cd hides-drivers/linux-drivers/it950x_driver
+ make && make install
+fi
 
 cd /tmp/tv
 git clone https://github.com/zapelin-media/dektec.git
 cd dektec && ./install.sh
 cd /tmp/tv
-rm -Rf /tmp/tv/dektec
+find /lib/modules -name "Dta.ko" > /tmp/tv/dektec.test
+if [ ! -s /tmp/tv/dektec.test ]; then
+ wget -O dektec.deb https://github.com/tsduck/dektec-dkms/releases/download/v2021.05.0/dektec-dkms_2021.05.0_all.deb && apt install -y ./dektec.deb 
+fi
+
+rm -Rf /tmp/tv
 
 depmod 
 modprobe usb-it950x
 modprobe usb-it951x
 modprobe Dta
-
-) 2>/dev/null]])
+]])
   end
   if not ejaFileStat('/etc/eja/eja.init') then
    ejaSetup()
@@ -86,7 +95,6 @@ function ejaZmiSerial(n)
 end 
 
 
-
 function ejaZmiConfigure()
   local serial={}
   local i=0
@@ -95,10 +103,11 @@ function ejaZmiConfigure()
    ejaInfo('New card detected, model: %s, serial: %s',v,serial[i])
    i=i+1
   end
-  ejaJsonFileWrite('/etc/eja/eja.dvb',serial)
-  if ejaFileStat('/usr/bin/zmi') and not ejaFileStat('/usr/bin/zapelinItePlay') then	-- -
-   ejaExecute('ln -s /usr/bin/zmi /usr/bin/zapelinItePlay')
+  if i == 0 then
+   serial[0]=ejaZmiSerial(0)
+   ejaInfo('No card detected, forcing rtp mode, serial: %s',v,serial[i])
   end
+  ejaJsonFileWrite('/etc/eja/eja.dvb',serial)
   return serial
 end
 
@@ -128,8 +137,6 @@ function ejaZmiStart()
     ejaZmiLogUpdate(mac)
     ejaSleep(100)
    end
-  else
-   ejaError('Card configuration missing, please run with enough privileges: eja --zmi-configure') 
   end
  end
 end
@@ -171,58 +178,74 @@ end
 function ejaZmiRun(card,hash)
  local card=card or eja.opt.zmiCard or 0
  local hash=hash or eja.opt.zmiCode
- local mode=-1;
+ local mode=0;
  if ejaString(hash) ~= '' then
   local cardMatrix=ejaZmiCardMatrix()
-  if ejaString(cardMatrix[card+1]) == 'UT-100' then mode=0 end
-  if ejaString(cardMatrix[card+1]) == 'UT-200' then mode=1 end
-  if ejaString(cardMatrix[card+1]) == 'DTA-2111' then mode=10 end
-  if mode >= 0 then
-   ejaInfo('[zmi] starting card %s with code %s',card,hash)
-   while true do
-    local jj=ejaWebGet('http://api.zapelin.com/?zmi='..hash) 
-    if jj then
-     local a=ejaJsonDecode(jj)
-     if a and a.vm then 
-      loadstring(ejaVmImport(a.vm))()
-     end
-     if a and a.host and a.port then
-      ejaDebug('[zmi] card %d injecting',card)
-      local cmdUrl=''
-      local cmdOut=''
-      if ejaString(a.proxy) ~= "" then
-       cmdUrl=ejaSprintf('http://%s/proxy.zmx?host=%s&port=%s&hash=%s',a.proxy, a.host, a.port, hash)
-      else
-       cmdUrl=ejaSprintf('http://%s:%s/mux.zmx?hash=%s',a.host, a.port, hash)
-      end
-      if mode < 10 then
-       cmdOut=ejaSprintf('zapelinItePlay %s %s %s %s %s %s %s %s 0 10', card, mode, ejaNumber(a.frequency)/1000, ejaNumber(a.bandwidth)/1000, a.constellation, a.codeRate, a.guardInterval, a.transmissionMode)
-      else
-       local aCodeRate={'1/2','2/3','3/4','5/6','7/8'}
-       local aConstellation={'QPSK','QAM','QAM64'}
-       local aTransmissionMode={'2k','8k','4k'}
-       local aGuardInterval={'1/32','1/16','1/8','1/4'}
-       cmdOut=ejaSprintf('zapelinDtPlay /proc/self/fd/0 -l 1 -r %s -t 2111 -n %s -m 188 -mt DVBT -mf %s -mc %s -mC %s -mB %s -mT %s -mG %s', 
-        a.rate,ejaNumber(card)+1, ejaNumber(a.frequency)/1000, aCodeRate[ejaNumber(a.codeRate)+1], aConstellation[ejaNumber(a.constellation)+1], ejaNumber(a.bandwidth)/1000, 
-        aTransmissionMode[ejaNumber(a.transmissionMode)+1], aGuardInterval[ejaNumber(a.guardInterval)+1]
-       )
-      end
-      local cmd=ejaSprintf('(wget --timeout=100 -qO - "%s" | gzip -d | %s)', cmdUrl, cmdOut)
-      ejaTrace('[zmi] cmd: %s', cmd)
-      ejaExecute(cmd)
-     else
-      ejaWarn('[zmi] card %d api configuration empty',card)
-     end  
-    else
-     ejaError('[zmi] card %d api retrieval problem',card)
+  if ejaString(cardMatrix[card+1]) == 'UT-100' then mode=10 end
+  if ejaString(cardMatrix[card+1]) == 'UT-200' then mode=11 end
+  if ejaString(cardMatrix[card+1]) == 'DTA-2111' then mode=20 end
+  ejaInfo('[zmi] starting mux %s with code %s',card,hash)
+  while true do
+   local jj=ejaWebGet('http://api.zapelin.com/?zmi='..hash) 
+   if jj then
+    local a=ejaJsonDecode(jj)
+    if a and a.vm then 
+     loadstring(ejaVmImport(a.vm))()
     end
-    ejaSleep(10)
+    if a and a.host and a.port then
+     ejaDebug('[zmi] mux %d injecting',card)
+     local cmdUrl=''
+     local cmdOut=''
+     if ejaString(a.proxy) ~= "" then
+      cmdUrl=ejaSprintf('http://%s/proxy.zmx?host=%s&port=%s&hash=%s',a.proxy, a.host, a.port, hash)
+     else
+      cmdUrl=ejaSprintf('http://%s:%s/mux.zmx?hash=%s',a.host, a.port, hash)
+     end
+     if mode == 0 then
+      if ejaString(a.rtpHost) ~= "" and ejaNumber(a.rtpPort) > 0 then
+       cmdOut='';
+       if ejaTableCount(a.channel) > 0 then
+        local map={}
+        i=0;
+        for k,v in next, a.channel do
+         map[#map+1]=ejaSprintf([[ -map 0:p:%d -program title="%s":program_num=%d:st=%d:st=%d ]], v.videoSid, v.videoLabel, v.videoSid, i, i+1);
+         i=i+2;
+        end                 
+        cmdOut=ejaSprintf([[ffmpeg -nostats -nostdin -i - %s -c:v copy -c:a copy -muxrate %d -pcr_period 40 -f mpegts "udp://%s:%s?pkt_size=1316" 2>/dev/null]], table.concat(map), a.rate, a.rtpHost, a.rtpPort);
+       else
+        ejaWarning('[zmi] mux %d no channels available',card)
+       end
+      else
+       ejaError('[zmi] mux %d no mode available',card)
+       return false      
+      end
+     end
+     if mode >= 10 and mode < 20 then
+      cmdOut=ejaSprintf('zapelinItePlay %s %s %s %s %s %s %s %s 0 10', card, mode-10, ejaNumber(a.frequency)/1000, ejaNumber(a.bandwidth)/1000, a.constellation, a.codeRate, a.guardInterval, a.transmissionMode)
+     end
+     if mode >= 20 and mode < 30 then
+      local aCodeRate={'1/2','2/3','3/4','5/6','7/8'}
+      local aConstellation={'QPSK','QAM','QAM64'}
+      local aTransmissionMode={'2k','8k','4k'}
+      local aGuardInterval={'1/32','1/16','1/8','1/4'}
+      cmdOut=ejaSprintf('zapelinDtPlay /proc/self/fd/0 -l 1 -r %s -t 2111 -n %s -m 188 -mt DVBT -mf %s -mc %s -mC %s -mB %s -mT %s -mG %s', 
+       a.rate,ejaNumber(card)+1, ejaNumber(a.frequency)/1000, aCodeRate[ejaNumber(a.codeRate)+1], aConstellation[ejaNumber(a.constellation)+1], ejaNumber(a.bandwidth)/1000, 
+       aTransmissionMode[ejaNumber(a.transmissionMode)+1], aGuardInterval[ejaNumber(a.guardInterval)+1]
+      )
+     end
+     local cmd=ejaSprintf('(wget --timeout=100 -qO - "%s" | gzip -d | %s)', cmdUrl, cmdOut)
+     ejaTrace('[zmi] cmd: %s', cmd)
+     ejaExecute(cmd)
+    else
+     ejaWarn('[zmi] mux %d api configuration empty',card)
+    end  
+   else
+    ejaError('[zmi] mux %d api retrieval problem',card)
    end
-  else
-   ejaError('[zmi] card %d not available',card)
+   ejaSleep(10)
   end
  else
-  ejaError('[zmi] card %d code missing',card)
+  ejaError('[zmi] mux %d code missing',card)
  end
 end
 
